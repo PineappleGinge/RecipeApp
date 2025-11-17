21e8fcf56a65af5973e8f0308a5207eb55f07eaa
Dara <daraorourke209@gmail.com>
2025-11-17 13:42:21 +0000

    added mainviremodle class with private val and fun toggleitem with an if statement

--- app/src/main/java/MainViewModel.kt
+++ app/src/main/java/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.recipeapp.navigation
 
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -11,3 +12,23 @@ data class ListItem(
     val title: String,
     val checked: Boolean = false
 )
+
+class MainViewModel : ViewModel(){
+
+    private val _shoppingItems = MutableStateFlow(
+        List(10) {index ->
+            ListItem(id = index, title = "List item ${index + 1}")
+        }
+    )
+    val shoppingItems: StateFlow<List<ListItem>> = _shoppingItems.asStateFlow()
+
+    fun toggleItem(id: Int){
+        _shoppingItems.value = _shoppingItems.value.map { item ->
+            if (item.id == id) {
+                item.copy(checked = !item.checked)
+            } else {
+                item
+            }
+        }
+    }
+}
