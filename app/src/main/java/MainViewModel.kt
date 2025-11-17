abba408c237443d7016ac4c36f5be9e43923864d
Dara <daraorourke209@gmail.com>
2025-11-17 13:32:48 +0000

    fixing some problems

---
+++ app/src/main/java/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.recipeapp.navigation
+
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class ListItem(
+    val id: Int,
+    val title: String,
+    val checked: Boolean = false
+)
