cfefa9a48144a08c9313bffc4bfb241f01d8af3c
Dara <daraorourke209@gmail.com>
2025-11-17 13:29:16 +0000

    added imports and listitem data class

--- app/src/main/java/MainViewModel.kt
+++ app/src/main/java/MainViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.recipeapp.navigation
 
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class ListItem(
+    val id: Int,
+    val title: String,
+    val checked: Boolean = false
+)
+
+
+
