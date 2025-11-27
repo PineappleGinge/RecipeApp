d5966654015d83add4d826c4ebcfcc30f61b7141
Dara <daraorourke209@gmail.com>
2025-11-27 12:33:42 +0000

    added code to RecipeSearchScreen.kt

--- app/src/main/java/com/example/recipeapp/ui/screens/RecipeSearchScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/screens/RecipeSearchScreen.kt
@@ -7,3 +7,48 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecipeSearchScreen(){
+    var text by remember { mutableStateOf("")}
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Text("Title", style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = {text = it},
+            label = { Text("Label")},
+            trailingIcon = {
+                if (text.isNotEmpty()){
+                    IconButton(onClick = {text = ""}) {
+                        Icon(Icons.Default.Close, contentDescription = "Clear")
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(modifier = Modifier.padding(16.dp)){
+                repeat(4){
+                    Text(
+                        "Menu Item",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+    }
+}
