38e08c98b701cb0db7d862f2fcc5ba2d826168c1
Dara <daraorourke209@gmail.com>
2025-11-27 17:03:00 +0000

    added code for recipe

--- app/src/main/java/com/example/recipeapp/data/local/Recipe.kt
+++ app/src/main/java/com/example/recipeapp/data/local/Recipe.kt
@@ -1,4 +1,12 @@
 package com.example.recipeapp.data.local
 
-class Recipe {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes")
+data class Recipe(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val imageUrl: String? = null,
+    val description: String? = null
+)
