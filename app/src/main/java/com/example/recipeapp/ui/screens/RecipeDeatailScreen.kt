022074a03361f60dcc7fabcd8d6c522b16597c5e
Dara <daraorourke209@gmail.com>
2025-11-27 12:43:17 +0000

    added imports to RecipeDeatailScreen.kt

--- app/src/main/java/com/example/recipeapp/ui/screens/RecipeDeatailScreen.kt
+++ app/src/main/java/com/example/recipeapp/ui/screens/RecipeDeatailScreen.kt
@@ -1,2 +1,16 @@
-package com.example.recipeapp.navigation.com.example.recipeapp.ui.screens
+package com.example.recipeapp.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.recipeapp.ListItem
 
